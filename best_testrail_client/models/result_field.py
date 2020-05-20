e5a32263a76a60160e20f8359628a159889e6014
LerikP <lerikpav@gmail.com>
2020-05-20 19:11:01 +0300

    Move helper classes near ResultField

--- best_testrail_client/models/result_field.py
+++ best_testrail_client/models/result_field.py
@@ -6,7 +6,27 @@ import typing
 from best_testrail_client.custom_types import ModelID, JsonData
 from best_testrail_client.enums import FieldType
 from best_testrail_client.models.basemodel import BaseModel
-from best_testrail_client.models.helpers import FieldConfig, Context, Options
+
+
+@dataclasses.dataclass
+class Context(BaseModel):
+    is_global: bool
+    project_ids: typing.Optional[typing.List[ModelID]]
+
+
+@dataclasses.dataclass
+class Options(BaseModel):
+    format: str   # noqa: VNE003, A003
+    has_actual: bool
+    has_expected: bool
+    is_required: bool
+
+
+@dataclasses.dataclass
+class FieldConfig(BaseModel):
+    id: ModelID  # noqa: A003, VNE003
+    context: Context
+    options: Options
 
 
 @dataclasses.dataclass
